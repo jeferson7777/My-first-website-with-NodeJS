c6d16a4bc5114618e4f0c1ece682a43660c37933
Jefersoon <jeffchc11@gmail.com>
2020-12-16 14:25:57 +0100

    contact page

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,4 +22,8 @@ router.get("/projects", (req, res) => {
   res.render("projects.html", { title: "Projects" });
 });
 
+router.get("/contacts", (req, res) => {
+  res.render("contacts.html", { title: "Contacts" });
+});
+
 module.exports = router;
